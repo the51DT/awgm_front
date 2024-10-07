736d700083628bd7d4bc70dad0fec3a233736f9f
100minkyung <mkbaek@the-51.com>
2024-10-07 17:59:03 +0900

    FrontIntro Lotti update

--- src/components/FrontIntro.vue
+++ src/components/FrontIntro.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="intro">Lotti 영역</div>
+  <div class="intro">
+    <div ref="animationContainer" style="width: 500px; height: 500px;"></div>
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import lottie from 'lottie-web';
+
+const animationContainer = ref(null);
+
+onMounted(() => {
+  lottie.loadAnimation({
+    container: animationContainer.value, // 애니메이션 들어갈 요소의 ref 값
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/6fe10512-efda-4a53-a2ed-29edb17fae6d/ujD16R84gk.json'
+  });
+});
+</script>
 
 <script>
 export default {
