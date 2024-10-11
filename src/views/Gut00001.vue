5c314c32bc0df4037f399a1ab6c4379a9a7dcf7f
sg51-lee <sglee@the-51.com>
2024-10-11 13:59:43 +0900

    작업내용 반영

---
+++ src/views/Gut00001.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content-top">
+    <div class="login-wrap">
+      <div class="login-intro">
+        <img class="ico-logo" :src="require(`@/assets/images/icon/ico_login01.png`)" alt="자녀계정 로그인">
+        <h4 class="ico-title">자녀계정 로그인</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginMain",
+};
+</script>
+
+<style scoped></style>
