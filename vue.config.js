35fff6513caf532158f2906aae25df1ff02fd41c
YB <ybjeong@the-51.com>
2024-10-11 18:11:34 +0900

    his

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  devServer: {
+    historyApiFallback: true,
+  },
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
