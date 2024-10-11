e4148ce20ab3b5d0cd5541710afd2ed855f2e464
100minkyung <mkbaek@the-51.com>
2024-10-11 19:29:31 +0900

    챌린지

--- src/layouts/AppHeader.vue
+++ src/layouts/AppHeader.vue
@@ -1,6 +1,19 @@
 <template>
   <header class="header">
-    <h1><FrontLogo /></h1>
+    <div class="header__left">
+      <h1><img :src="require(`@/assets/images/header_logo.svg`)"></h1>
+      <div class="header__left--cont">
+        <img :src="require(`@/assets/images/header_text.svg`)">
+        <div class="header__left--text">누적 <span>128만</span>회(<span>10.5만</span>명), 오늘 <span>7.8천</span>회</div>
+      </div>
+    </div>
+    <div class="header__right">
+      <template v-if="mypage">
+        <img :src="require(`@/assets/images/header_info.svg`)">
+        <img :src="require(`@/assets/images/header_faq.svg`)">
+      </template>
+      <img v-else class="header__right--notice" :src="require(`@/assets/images/header_notice.svg`)">
+    </div>
   </header>
 </template>
 
@@ -11,8 +24,11 @@ import FrontLogo from "@/components/FrontLogo.vue";
 export default {
   name: "AppHeader",
   components: {
-    FrontLogo,
+    // FrontLogo,
   },
+  props: {
+    mypage: Boolean,
+  }
 };
 </script>
 <style scoped></style>
--- src/views/Gut0000701.vue
+++ src/views/Gut0000701.vue
@@ -1,21 +1,59 @@
 <template>
-  <div class="content-top">
-    <div class="login-wrap">
-      <div class="login-intro">
-        <img
-          class="ico-logo"
-          :src="require(`@/assets/images/icon/ico_login01.png`)"
-          alt="자녀계정 로그인"
-        />
-        <h4 class="ico-title">자녀계정 로그인</h4>
+  <!-- 기본 -->
+  <!-- <div class="popup">
+    <div class="popup--img">
+      <button class="popup--img__close"></button>
+      <div class="card--img">
+        <img class="card--img__comp" :src="require(`@/assets/images/challenge_comp.png`)">
+        <img :src="require(`@/assets/images/challenge_card_bg.png`)">
+        <div>
+          <p>2024.09.23 (월)</p>
+          <p>나의 미션 인증 횟수</p>
+          <p><span>2</span>회</p>
+        </div>
+        <button class="share-btn"></button>
+      </div>
+    </div>
+  </div> -->
+  <!-- 공유하기 버튼 클릭 시 -->
+  <div class="popup--pull">
+    <div class="popup--pull__top">
+      <p>챌린지 이력보기</p>
+      <button class="popup--pull__close"></button>
+    </div>
+    <div class="popup--pull__cont">
+      <div class="card--img">
+        <img class="card--img__comp" :src="require(`@/assets/images/challenge_comp.png`)">
+        <img :src="require(`@/assets/images/challenge_card_bg.png`)">
+        <div>
+          <p>2024.09.23 (월)</p>
+          <p>나의 미션 인증 횟수</p>
+          <p><span>2</span>회</p>
+        </div>
+        <p><span>팀장김웨이</span>님의<br />GUT Morning</p>
+      </div>
+      <div class="card card--border">
+        <div class="calender">
+          캘린더 영역
+        </div>
+      </div>
+      <div>
+        <CustomButton color="yellow">카카오로 공유</CustomButton>
+        <CustomButton>전체 이미지 저장</CustomButton>
+        <CustomButton>오늘 인증 이미지 저장</CustomButton>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CustomButton from "@/components/CustomButton.vue";
+
 export default {
   name: "LoginMain",
+  components: {
+    CustomButton,
+  }
 };
 </script>
 
