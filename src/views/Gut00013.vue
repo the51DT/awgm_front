94f9c8ffe9db6cc9991f024f069cddc4271b7607
sg51-lee <sglee@the-51.com>
2024-10-14 19:14:01 +0900

    작업 내용 수정사항 선반영

--- src/views/Gut00013.vue
+++ src/views/Gut00013.vue
@@ -1,22 +1,99 @@
 <template>
-  <div class="content-top">
-    <div class="login-wrap">
-      <div class="login-intro">
-        <img
-          class="ico-logo"
-          :src="require(`@/assets/images/icon/ico_login01.png`)"
-          alt="자녀계정 로그인"
-        />
-        <h4 class="ico-title">자녀계정 로그인</h4>
+  <div class="content-area">
+    <ContentHead title="팀원" headType="ty02" :leftArea=true />
+    <div class="team-box-wrap">
+      <div class="card card--border card--p16 card--shadowc2">
+        <a href="#" class="card--link">
+          <div class="card__top">
+            <div class="card__profile__info"> 
+              <!-- 팀장일 경우 on-flag 클래스 추가시 왕관 아이콘 활성화됨 -->
+              <div class="img-wrap on-flag">
+                <img class="card__img" :src="require(`@/assets/images/card_profile.png`)">
+              </div>
+              <p class="card__tit">팀장김웨이팀장김웨이팀장김웨이팀장김웨이팀장김웨이팀장김웨이팀장김웨이</p>
+            </div>
+            <div class="card__text__list">
+              <dl>
+                <dt>랭킹</dt>
+                <dd>1위</dd>
+              </dl>
+              <dl>
+                <dt>팀 가입일</dt>
+                <dd>2024.09.09</dd>
+              </dl>
+              <dl>
+                <dt>인증 횟수</dt>
+                <dd>
+                  <div class="group-info">
+                    <p class="main">223/1000회</p>
+                    <p class="sub">($9$월 30회)</p>
+                  </div>
+                </dd>
+              </dl>
+              <dl class="align-ctr">
+                <dt>ABO/Member</dt>
+                <dd>
+                  <p class="badge-info">인증</p>
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="card card--border card--p16 card--shadowc2">
+        <a href="#" class="card--link">
+          <div class="card__top">
+            <div class="card__profile__info"> 
+              <div class="img-wrap">
+                <img class="card__img" :src="require(`@/assets/images/card_profile.png`)">
+              </div>
+              <p class="card__tit">김사무엘 유리카 딕슨 김사무엘 유리카 딕슨김사무엘 유리카 딕슨김사무엘 유리카 딕슨김사무엘 유리카 딕슨</p>
+            </div>
+            <div class="card__text__list">
+              <dl>
+                <dt>랭킹</dt>
+                <dd>2위</dd>
+              </dl>
+              <dl>
+                <dt>팀 가입일</dt>
+                <dd>2024.09.09</dd>
+              </dl>
+              <dl>
+                <dt>인증 횟수</dt>
+                <dd>
+                  <div class="group-info">
+                    <p class="main">223/1000회</p>
+                    <p class="sub">($9$월 30회)</p>
+                  </div>
+                </dd>
+              </dl>
+              <dl class="align-ctr">
+                <dt>ABO/Member</dt>
+                <dd>
+                  <p class="badge-info">인증</p>
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ContentHead from "@/components/ContentHead.vue";
+
 export default {
-  name: "LoginMain",
+  name: "Gut_00012",
+  data() {
+    return {
+      leftArea: "",
+      rightArea: "",
+    };
+  },
+  components: {
+    ContentHead,
+  },
 };
 </script>
-
-<style scoped></style>
