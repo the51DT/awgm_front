a0057d5c711bae2b76d2b042389837fa8810fa9a
sg51-lee <sglee@the-51.com>
2024-10-14 14:24:23 +0900

    ContentHead 컴포넌트 생성

---
+++ src/components/ContentHead.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="head-area"
+  :class="[
+    headType === 'ty02' ? 'ty02' : '',
+  ]">
+    <a class="area-left" href="#" v-if="leftArea === true">
+      <img class="ico-logo"
+        :src="require(`@/assets/images/icon/ico_arrow_left.png`)"
+        alt="이전페이지로 이동"
+      />
+    </a>
+    <h2 class="page-title">{{ title }}</h2>
+    <button type="button" class="area-right" v-if="rightArea === true">
+      <img class="ico-logo"
+        :src="require(`@/assets/images/icon/ico_close.png`)"
+        alt="닫기"
+      />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentHead",
+  props: {
+    title: String,
+    headType: String,
+    leftArea : Boolean,
+    rightArea : Boolean,
+  }
+}
+</script>
--- src/views/Gut00012.vue
+++ src/views/Gut00012.vue
@@ -1,22 +1,46 @@
 <template>
-  <div class="content-top">
-    <div class="login-wrap">
-      <div class="login-intro">
-        <img
-          class="ico-logo"
-          :src="require(`@/assets/images/icon/ico_login01.png`)"
-          alt="자녀계정 로그인"
-        />
-        <h4 class="ico-title">자녀계정 로그인</h4>
+  <div class="content-area">
+    <ContentHead title="챌린지 팀 관리" headType="ty02" :leftArea=true />
+    <div class="login-form">
+      <div class="form-box">
+        <h4 class="title">부모계정 인증</h4>
+        <ul class="info--list">
+          <li>* 자녀계정일 경우 부모의 동의가 필요합니다.</li>
+        </ul>
+        <div class="input-area">
+          <InputField placeholder="ABO 번호/Member 아이디" />
+          <InputField type="password" placeholder="비밀번호" />
+        </div>
+        <div class="btn-area ty02">
+          <p class="label-txt">인증이 완료되었습니다.</p>
+          <CustomButton size="sm" color="gray">인증</CustomButton>
+        </div>
+      </div>
+      <div class="btn-group">
+        <CustomButton color="dark" class="ty-chk"><span>법정대리인 개인정보 활용 동의</span></CustomButton>
+        <CustomButton>로그인</CustomButton>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ContentHead from "@/components/ContentHead.vue";
+import InputField from "@/components/InputField.vue";
+import CustomButton from "@/components/CustomButton.vue";
+
 export default {
-  name: "LoginMain",
+  name: "Gut_00012",
+  data() {
+    return {
+      leftArea: "",
+      rightArea: "",
+    };
+  },
+  components: {
+    ContentHead,
+    InputField,
+    CustomButton
+  },
 };
 </script>
-
-<style scoped></style>
