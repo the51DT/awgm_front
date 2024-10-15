b1cf6d5a8afca391ff6811d5afa601099826f48a
sg51-lee <sglee@the-51.com>
2024-10-15 16:39:43 +0900

    10.15 작업 수정사항 반영
    - 10.15 나의 팀 잔여 페이지 작업 수정사항 반영

--- src/views/Gut00014.vue
+++ src/views/Gut00014.vue
@@ -1,82 +1,91 @@
 <template>
   <div class="content-area bg-case">
     <ContentHead title="팀 만들기" headType="ty02" :leftArea=true />
-    <div class="team-box-wrap">
-      <div class="profile-area">
-        <div class="upload-wrap">
-          <input type="file" id="upload-icon" hidden />
-          <label for="upload-icon" class="upload-label">
-            <img class="ico-logo" :src="require(`@/assets/images/icon/ico_file.png`)" alt="파일 업로드 등록 아이콘" />
-          </label>
+    <div class="create-team-wrap">
+      <div class="profile-wrap">
+        <div class="profile-area">
+          <div class="upload-wrap">
+            <input type="file" id="uploadIcon" class="upload-icon" hidden />
+            <label for="uploadIcon" class="upload-label">
+              <img class="ico-logo" :src="require(`@/assets/images/icon/ico_file.png`)" alt="파일 업로드 등록 아이콘" />
+            </label>
+          </div>
         </div>
       </div>
       <div class="card card--border card--p16 card--shadowc2 ">
-        <div class="form-box">
-          <div class="input-area">
-            <div class="form-item">
-              <InputField title="팀명" placeholder="팀명을 입력해 주세요."/>
+        <div class="input-area">
+          <div class="form-item">
+            <InputField title="팀명" placeholder="팀명을 입력해 주세요."/>
+          </div>
+          <div class="form-item">
+            <h5 class="title">팀 구분</h5>
+            <div class="radio-wrap">
+              <InputField type="radio" title="" name="radio1"
+              :radio="[
+                {id: 'radio1_1', value: 'radio1_1', checked: true, label: '챌린지'},
+                {id: 'radio1_2', value: 'radio1_2', checked: false, label: '상시'},
+              ]" />
             </div>
-            <div class="form-item">
-              <h5 class="title">팀 구분</h5>
-              <p class="desc">챌린지</p>
+          </div>
+          <!-- 기간 : type1 -->
+          <div class="form-item">
+            <h5 class="title">기간</h5>
+            <p class="desc">2025.09.01 ~ 2025.11.15 (70일)</p>
+          </div>
+          <!-- 기간 : type2 -->
+          <div class="form-item">
+            <InputField type="date" title="기간"
+              :date="[
+                {value: '2024-09-30', disabled: false},
+                {value: '2024-09-30', disabled: false, placeholder: '선택'},
+              ]" />
+            <div class="form-item--sub">
+              <InputField type="checkbox" name="checkbox1"
+                :checkbox="[
+                  {id: 'checkbox1_1', value: 'checkbox1_1', checked: true, label: '종료일 없음'},
+                ]"  />
             </div>
-            <!-- 기간 : type1 -->
-            <div class="form-item">
-              <h5 class="title">기간</h5>
-              <p class="desc">2025.09.01 ~ 2025.11.15 (70일)</p>
+          </div>
+          <!-- ABC 수령 case -->
+          <div class="form-item">
+            <h5 class="title">성공 보상</h5>
+            <p class="desc">$보상명$</p>
+          </div>
+          <div class="form-item">
+            <h5 class="title">보상수령방법</h5>
+            <div class="radio-wrap">
+              <InputField type="radio" title="" name="radio2"
+              :radio="[
+                {id: 'radio2_1', value: 'radio2_1', checked: true, label: '팀장이 수령'},
+                {id: 'radio2_2', value: 'radio2_2', checked: false, label: '팀별 개별수령'},
+              ]" />
             </div>
-            <!-- 기간 : type2 -->
-            <div class="form-item">
-              <InputField type="date" title="기간"
-                :date="[
-                  {value: '2024-02-14', disabled: true},
-                  {disabled: false, placeholder: '선택'},
-                ]" />
-              <div class="form-item--sub">
-                <InputField type="checkbox" name="checkbox1"
-                  :checkbox="[
-                    {id: 'checkbox1_1', value: 'checkbox1_1', checked: true, label: '라벨'},
-                  ]"  />
-              </div>
-            </div>
-            <div class="form-item">
-              <h5 class="title">챌린지 시작일</h5>
-              <p class="desc">2025.09.12</p>
-            </div>
-            <div class="form-item">
-              <h5 class="title">참여 가능 팀원</h5>
-              <p class="desc">5 ~ 99</p>
-            </div>
-            <div class="form-item">
-              <h5 class="title">성공조건</h5>
-              <p class="desc"><span class="font--yellow">80%</span> (인증횟수 300회 이상)</p>
-            </div>
-            <div class="form-item">
-              <h5 class="title">성공 보상</h5>
-              <p class="desc">$보상명$</p>
-            </div>
-            <div class="form-item">
-              <h5 class="title">보상수령방법</h5>
-              <div class="radio-wrap">
-                <InputField type="radio" title="" name="radio1"
-                :radio="[
-                  {id: 'radio1_1', value: 'radio1_1', checked: true, label: '팀장이 수령'},
-                  {id: 'radio1_2', value: 'radio1_2', checked: false, label: '팀별 개별수령'},
-                ]" />
-              </div>
-            </div>
-            <div class="form-item">
-              <h5 class="title">보상수령처</h5>
-              <div class="select-wrap">
-                <InputField type="select" placeholder="선택" :options="['값1', '값2', '값3']" />
-              </div>            
-            </div>
-          </div>          
+          </div>
+          <div class="form-item">
+            <h5 class="title">보상수령처</h5>
+            <div class="select-wrap">
+              <InputField type="select" placeholder="선택" :options="['값1', '값2', '값3']" />
+            </div>            
+          </div>
+          <!-- //ABC 수령 case -->
+          <!-- 바우처 case -->
+          <div class="form-item">
+            <h5 class="title">성공조건</h5>
+            <p class="desc"><span class="font--yellow">80% 이상</span> (인증횟수 300회 이상)</p>
+          </div>
+          <div class="form-item">
+            <h5 class="title">참여 가능 팀원</h5>
+            <p class="desc">5 ~ 99</p>
+          </div>
+          <div class="form-item">
+            <h5 class="title">보상</h5>
+            <p class="desc">$바우처명$ 바우처</p>
+          </div>
+          <!-- 바우처 case -->
         </div>
       </div>
-      <div class="btn-area ty03">
-        <CustomButton color="light" class="ty02">팀원</CustomButton>
-        <CustomButton>수정</CustomButton>
+      <div class="btn-area">
+        <CustomButton>완료</CustomButton>
       </div>
     </div>
   </div>
--- src/views/Gut00032.vue
+++ src/views/Gut00032.vue
@@ -2,35 +2,37 @@
   <div class="content-area">
     <ContentHead title="팀원 관리" headType="ty02" :leftArea=true />
     <div class="team-box-wrap">
-      <div class="profile-area small">
-        <div class="img-wrap on-flag"> <!-- 팀장일 경우 on-flag 클래스 추가시 왕관 아이콘 활성화됨 -->
-          <img class="ico-logo" :src="require(`@/assets/images/content/dummy-profile-img.png`)" alt="프로필 아이콘" />
-        </div>
-        <div class="text-wrap">
-          <h4 class="profile-name">김사무엘 유리카 닉슨</h4>
-        </div>
-      </div>
-      <div class="card card--border card--p16 card--shadowc2">
-        <div class="card__top">
-          <div class="card__text__list">
-            <dl>
-              <dt>팀 가입일</dt>
-              <dd>2024.09.09</dd>
-            </dl>
-            <dl>
-              <dt>인증 횟수</dt>
-              <dd>223/1000회 (9월 22회)</dd>
-            </dl>
-            <dl class="align-ctr">
-              <dt>ABO/Member</dt>
-              <dd>
-                <p class="badge-info">인증</p>
-              </dd>
-            </dl>
+      <div class="conts-area">
+        <div class="profile-area small">
+          <div class="img-wrap on-flag"> <!-- 팀장일 경우 on-flag 클래스 추가시 왕관 아이콘 활성화됨 -->
+            <img class="ico-logo" :src="require(`@/assets/images/content/dummy-profile-img.png`)" alt="프로필 아이콘" />
+          </div>
+          <div class="text-wrap">
+            <h4 class="profile-name">김사무엘 유리카 닉슨</h4>
+          </div>
+        </div>        
+        <div class="card card--border card--p16 card--shadowc2">
+          <div class="card__top">
+            <div class="card__text__list">
+              <dl>
+                <dt>팀 가입일</dt>
+                <dd>2024.09.09</dd>
+              </dl>
+              <dl>
+                <dt>인증 횟수</dt>
+                <dd>223/1000회 (9월 22회)</dd>
+              </dl>
+              <dl class="align-ctr">
+                <dt>ABO/Member</dt>
+                <dd>
+                  <p class="badge-info">인증</p>
+                </dd>
+              </dl>
+            </div>
           </div>
         </div>
+        <div class="calender"> 캘린더 영역 </div>
       </div>
-      <div class="calender"> 캘린더 영역 </div>
       <div class="team-btn-wrap">
         <div class="btn-area ty03">
           <CustomButton color="light">강퇴</CustomButton>
