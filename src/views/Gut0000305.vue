a3f80f17ae2bd8f61b7a46c7a33b167933a8bc9f
100minkyung <mkbaek@the-51.com>
2024-11-18 14:51:32 +0900

    문구수정

--- src/views/Gut0000305.vue
+++ src/views/Gut0000305.vue
@@ -7,7 +7,7 @@
       <div class="box-wrap">
         <div class="box-gray">
           <div class="terms-conts">
-            미성년자녀의 것모님 (Gut Morning) 서비스 내 팀 챌린지 참가에 동의합니다.<br/>
+            미성년자녀의 것모닝 (Gut Morning) 서비스 내 팀 챌린지 참가에 동의합니다.<br/>
             가입을 신청한 팀장은 귀하의 자녀의 닉네임과 인증 횟수를 확인할 수 있습니다.<br/>
             귀하의 자녀의 활동 정보를 대시 보드에서 볼 수 있습니다.<br/>
           </div>
