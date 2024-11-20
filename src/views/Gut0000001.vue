394790a8270597417c761a14d264504b4c86dfd3
100minkyung <mkbaek@the-51.com>
2024-11-20 11:05:16 +0900

    팝업 템플릿 태그 변경

--- src/views/Gut0000001.vue
+++ src/views/Gut0000001.vue
@@ -6,9 +6,9 @@
     <template v-slot:cont>
       <div class="popup__scroll">
         <img src="https://placehold.co/600x1200">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, harum! Assumenda temporibus natus dicta, beatae maxime voluptas repellat eveniet magni ratione illo enim corrupti nemo illum molestiae, maiores harum officiis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta excepturi nobis dignissimos, dolor error, a temporibus ipsum vel iusto sapiente non tempora optio quas esse nemo molestiae possimus, impedit explicabo? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rerum sequi minima quaerat unde eos expedita ratione obcaecati, laborum nesciunt ipsum aliquam. Iure doloremque fuga cum? Iusto quibusdam tempore doloremque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolore cupiditate, possimus aperiam, eligendi, dolor facere provident necessitatibus delectus modi beatae eum! Optio quibusdam quos iste excepturi incidunt quo natus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum architecto, quam quis voluptates libero nesciunt placeat in magnam excepturi et beatae delectus voluptate inventore necessitatibus ducimus fugit at. Porro, earum.
-        </p>
+        <div>
+          Lorem ipsum dolor sit ame
+        </div>
       </div>
     </template>
     <template v-slot:btn>
