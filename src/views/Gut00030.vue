1d1b917e5044d2c633b418ea89bcaad0e48d9c67
100minkyung <mkbaek@the-51.com>
2024-12-04 17:32:08 +0900

    사파리 네비 스크롤 테스트용

--- src/views/Gut00030.vue
+++ src/views/Gut00030.vue
@@ -1,22 +1,23 @@
 <template>
-  
-  <div class="content-etc-wrap"> 
-    <div class="content-etc-box">
-      <div class="content-etc-img-area">
-        <img :src="require('@/assets/images/icon/ico_etc03.png')" alt="500 에러페이지 아이콘">
+  <div class="content-etc-area">
+    <div class="content-etc-wrap"> 
+      <div class="content-etc-box">
+        <div class="content-etc-img-area">
+          <img :src="require('@/assets/images/icon/ico_etc03.png')" alt="500 에러페이지 아이콘">
+        </div>
+        <div class="content-etc-text-area">
+          <h2 class="page-title">
+            500 Unexpected Error
+            <strong class="sub-title">서비스에 접속할 수 없습니다</strong>
+          </h2>
+          <p class="page-desc">
+            기술적인 문제로 일시적으로 접속되지 않았습니다. <br/>잠시 후 다시 한번 확인하여 주시기 바랍니다.
+          </p>
+        </div>      
       </div>
-      <div class="content-etc-text-area">
-        <h2 class="page-title">
-          500 Unexpected Error
-          <strong class="sub-title">서비스에 접속할 수 없습니다</strong>
-        </h2>
-        <p class="page-desc">
-          기술적인 문제로 일시적으로 접속되지 않았습니다. <br/>잠시 후 다시 한번 확인하여 주시기 바랍니다.
-        </p>
-      </div>      
+      <CustomButton>재시작</CustomButton>  
     </div>
-    <CustomButton>재시작</CustomButton>  
-  </div>  
+  </div> 
 </template>
 
 <script>
--- src/views/Gut00031.vue
+++ src/views/Gut00031.vue
@@ -1,24 +1,25 @@
 <template>
-  
-  <div class="content-etc-wrap"> 
-    <div class="content-etc-box">
-      <div class="content-etc-img-area">
-        <img :src="require('@/assets/images/icon/ico_etc04.png')" alt="404 에러페이지 아이콘">
+  <div class="content-etc-area">
+    <div class="content-etc-wrap"> 
+      <div class="content-etc-box">
+        <div class="content-etc-img-area">
+          <img :src="require('@/assets/images/icon/ico_etc04.png')" alt="404 에러페이지 아이콘">
+        </div>
+        <div class="content-etc-text-area">
+          <h2 class="page-title">
+            404 Page Not Found
+            <strong class="sub-title">페이지를 표시할 수 없습니다</strong>
+          </h2>
+          <p class="page-desc">
+            찾으려는 페이지는 존재하지 않는 페이지입니다. <br/>
+            입력하신 페이지 주소가 정확한지 <br/>
+            다시 한번 확인하여 주시기 바랍니다. 
+          </p>
+        </div>      
       </div>
-      <div class="content-etc-text-area">
-        <h2 class="page-title">
-          404 Page Not Found
-          <strong class="sub-title">페이지를 표시할 수 없습니다</strong>
-        </h2>
-        <p class="page-desc">
-          찾으려는 페이지는 존재하지 않는 페이지입니다. <br/>
-          입력하신 페이지 주소가 정확한지 <br/>
-          다시 한번 확인하여 주시기 바랍니다. 
-        </p>
-      </div>      
+      <CustomButton>홈으로</CustomButton>  
     </div>
-    <CustomButton>홈으로</CustomButton>  
-  </div>  
+  </div>
 </template>
 
 <script>
