26d279c6506239d020b573ccc6e579d13fa4c28e
YB <ybjeong@the-51.com>
2024-12-06 17:48:45 +0900

    ㄴㄴ

--- src/layouts/AppSidebar.vue
+++ src/layouts/AppSidebar.vue
@@ -1,39 +1,26 @@
 <template>
-  <v-list class="menu-list lnb_front">
+  <div class="menu-list lnb_front">
     <template v-for="(item, index) in menuRoutes" :key="index">
-      <v-list-item
-        :class="[
-          'list_1dep',
-          { select: isSelected(item), completed: item.isCompleted },
-        ]"
-        @click="handleClick(item)"
-      >
-        <v-list-item-content>
-          <v-list-item-title class="btn_menu">
-            {{ item.text }}
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <div class="list_1dep" @click="handleClick(item)">
+        <div class="btn_menu">
+          {{ item.text }}
+        </div>
+      </div>
 
-      <v-list class="menu_2dep" v-if="item.subMenu">
-        <v-list-item
+      <div class="menu_2dep" v-if="item.subMenu">
+        <div
           v-for="(subItem, subIndex) in item.subMenu"
           :key="subIndex"
-          :class="[
-            'list_2dep',
-            { select: isSelected(subItem), completed: subItem.isCompleted },
-          ]"
+          class="list_2dep"
           @click="handleClick(subItem)"
         >
-          <v-list-item-content>
-            <v-list-item-title class="btn_menu">
-              {{ subItem.text }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+          <div class="btn_menu">
+            {{ subItem.text }}
+          </div>
+        </div>
+      </div>
     </template>
-  </v-list>
+  </div>
 </template>
 
 <script setup>
@@ -47,20 +34,8 @@ const handleClick = (item) => {
     router.push(item.path);
   }
 };
-
-// eslint-disable-next-line no-unused-vars
-const isSelected = (item) => {
-  return false;
-};
 </script>
 <style scoped>
-.v-btn {
-  margin-left: 8px;
-}
-
-.v-icon {
-  font-size: 20px;
-}
 .lnb_front {
   background: transparent;
 
@@ -71,36 +46,19 @@ const isSelected = (item) => {
   .menu-list::-webkit-scrollbar {
     display: none;
   }
-  .v-list {
-    padding: 0;
-    background: transparent;
-    display: block;
-
-    .v-list-item {
-      min-height: 40px;
-      white-space: normal;
-      display: block;
-      margin-bottom: 8px;
-    }
-    .v-list-item-title {
-      white-space: normal;
-    }
-    .v-list-item.list_1dep {
-      min-height: 50px;
-      background: #3d4a62;
-      margin-bottom: 12px;
-      border-radius: 8px;
-      display: block;
-    }
-  }
 
   .list_1dep {
+    background: #3d4a62;
+    margin-bottom: 12px;
+    border-radius: 8px;
+    min-height: 50px;
+
     .btn_menu {
       position: relative;
       display: block;
       font-size: 18px;
       font-weight: 700;
-      color: #333;
+      color: #fff;
       padding: 15px;
     }
   }
@@ -108,10 +66,8 @@ const isSelected = (item) => {
   .menu_2dep {
     background: transparent;
     padding-left: 20px;
-    display: block;
 
     .list_2dep {
-      display: block;
       margin-bottom: 8px;
 
       .btn_menu {
@@ -125,10 +81,14 @@ const isSelected = (item) => {
     }
   }
 }
+
+.v-btn,
+.v-icon,
 .completed {
-  /* 페이지 완료 여부용
-  text-decoration: line-through;
-  text-decoration-color: #fff;
-  font-weight: bold; */
+  display: none !important;
+}
+
+.stamp-effect {
+  display: none !important;
 }
 </style>
