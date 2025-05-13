5a6bfd09ba256a52b7792ba6e50b8ade6673727d
suzy hong <victoria1230120@gmail.com>
2025-05-13 10:26:21 +0900

    ChildIcon 컴포넌트 추가
    - Gut00019에 있던 기존 마크업 수정

---
+++ src/components/ChildIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my__list__img">
+    <img :src="require(`@/assets/images/child_profile${img_num}.png`)" alt="child Icon" />
+  </div>
+</template>
+
+<script setup>
+// img_num : 숫자만
+defineProps({
+  img_num: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
--- src/views/Gut00019.vue
+++ src/views/Gut00019.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="content-area">
-    <ContentHead title="자녀계정 등록/관리" :leftArea=true />
+    <ContentHead title="자녀계정 등록/관리" :leftArea="true" />
     <!-- 자녀계정이 있는 경우 -->
     <div class="my__list">
-      <p class="my__list__title">팀장김웨이님은<br/><span>총 4개</span>의 자녀계정이 있습니다.</p>
+      <p class="my__list__title">
+        팀장김웨이님은<br /><span>총 4개</span>의 자녀계정이 있습니다.
+      </p>
       <div class="my__list--items">
         <div class="card card--border card--p16 card--shadow00">
           <button class="favorites_btn on"></button>
           <div class="card__top">
-            <div class="my__list__img">
-              <img :src="require(`@/assets/images/child_profile1.png`)">
-            </div>
+            <ChildIcon img_num="1" />
             <div class="my__list__text">
               <p class="id">qwert123456</p>
               <p class="name">니노막시무스카에저소제소냐도르앤스파르타</p>
@@ -26,9 +26,7 @@
         <div class="card card--border card--p16 card--shadow00">
           <button class="favorites_btn"></button>
           <div class="card__top">
-            <div class="my__list__img">
-              <img :src="require(`@/assets/images/child_profile2.png`)">
-            </div>
+            <ChildIcon img_num="2" />
             <div class="my__list__text">
               <p class="id">qwert123456</p>
               <p class="name">니노막시무스카에저소제소냐도르앤스파르타</p>
@@ -44,9 +42,7 @@
         <div class="card card--border card--p16 card--shadow00">
           <button class="favorites_btn"></button>
           <div class="card__top">
-            <div class="my__list__img">
-              <img :src="require(`@/assets/images/child_profile3.png`)">
-            </div>
+            <ChildIcon img_num="3" />
             <div class="my__list__text">
               <p class="id">qwert123456</p>
               <p class="name">니노막시무스카에저소제소냐도르앤스파르타</p>
@@ -62,9 +58,7 @@
         <div class="card card--border card--p16 card--shadow00">
           <button class="favorites_btn"></button>
           <div class="card__top">
-            <div class="my__list__img">
-              <img :src="require(`@/assets/images/child_profile4.png`)">
-            </div>
+            <ChildIcon img_num="4" />
             <div class="my__list__text">
               <p class="id">qwert123456</p>
               <p class="name">니노막시무스카에저소제소냐도르앤스파르타</p>
@@ -80,22 +74,25 @@
         <div class="card card--border card--p16 card--shadow00">
           <button class="delete_btn"></button>
           <div class="card__top">
-            <div class="my__list__img">
-              <img :src="require(`@/assets/images/child_profile5.png`)">
-            </div>
+            <ChildIcon img_num="5" />
             <div class="my__list__text">
               <p class="incomplete">미완료 계정 ①</p>
-              <p class="explain">계정 등록 링크를 통해 자녀가 직접 계정 정보를 입력해야 계정 생성이 완료됩니다.</p>
+              <p class="explain">
+                계정 등록 링크를 통해 자녀가 직접 계정 정보를 입력해야 계정
+                생성이 완료됩니다.
+              </p>
             </div>
           </div>
           <div class="btn_wrap">
-            <CustomButton color="white">자녀계정 등록 링크 다시 공유</CustomButton>
+            <CustomButton color="white"
+              >자녀계정 등록 링크 다시 공유</CustomButton
+            >
           </div>
         </div>
         <div class="card card--border card--p16 card--shadow00">
           <div class="card__top">
             <div class="my__list__img">
-              <img :src="require(`@/assets/images/child_profile5.png`)">
+              <img :src="require(`@/assets/images/child_profile5.png`)" />
             </div>
             <div class="my__list__text">
               <p class="incomplete">자녀 계정 ID</p>
@@ -106,7 +103,10 @@
         </div>
       </div>
       <div class="my__list--add">
-        <CustomButton>자녀계정 생성하기 <img :src="require(`@/assets/images/icon_plus_white.svg`)"></CustomButton>
+        <CustomButton
+          >자녀계정 생성하기
+          <img :src="require(`@/assets/images/icon_plus_white.svg`)"
+        /></CustomButton>
       </div>
     </div>
 
@@ -123,6 +123,7 @@
 <script>
 import ContentHead from "@/components/ContentHead.vue";
 import CustomButton from "@/components/CustomButton.vue";
+import ChildIcon from "../components/ChildIcon.vue";
 
 export default {
   name: "LoginMain",
@@ -134,6 +135,7 @@ export default {
   components: {
     ContentHead,
     CustomButton,
+    ChildIcon,
   },
 };
 </script>
