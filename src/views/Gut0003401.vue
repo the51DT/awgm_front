378a017d8416d73f3ac93fb4559fd8c71b0d64dd
suzy hong <victoria1230120@gmail.com>
2025-06-04 14:28:04 +0900

    스타일링 추가
    - IconButton
    - 팀 목록

---
+++ src/views/Gut0003401.vue
@@ -0,0 +1,36 @@
+<template>
+  <PopupDefault>
+    <template v-slot:title>
+      이용약관 동의
+    </template>
+    <template v-slot:cont>
+
+      <ul class="info--list">
+        <li>원활한 서비스 이용을 위하여 개인정보 제3자 제공에 대하여 동의가 필요합니다. 동의하지 않으실 경우 서비스 이용이 제한됩니다.</li>
+      </ul>
+    </template>
+    <template v-slot:btn>
+      <div class="btn-area ty03">
+        <CustomButton size="sm" color="light">동의하지 않음</CustomButton>
+        <CustomButton size="sm">동의</CustomButton>
+      </div>
+    </template>
+  </PopupDefault> 
+</template>
+
+<script>
+import PopupDefault from "@/components/PopupDefault.vue";
+import CustomButton from "@/components/CustomButton.vue";
+import layerClose from "@/plugins/common.js";
+
+export default {
+  name: "AppSub",
+  components: {    
+    PopupDefault,
+    CustomButton,
+  },
+  mounted() {
+    layerClose.layerClose("popup");
+  }
+};
+</script>
