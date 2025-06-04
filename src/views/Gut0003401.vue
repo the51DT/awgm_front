141e842a702884b5607e1d17c9252a5c288a3cc5
suzy hong <victoria1230120@gmail.com>
2025-06-04 14:39:49 +0900

    나의 팀 목록 초대받은팀 팝업

--- src/views/Gut0003401.vue
+++ src/views/Gut0003401.vue
@@ -1,18 +1,16 @@
 <template>
   <PopupDefault>
-    <template v-slot:title>
-      이용약관 동의
-    </template>
     <template v-slot:cont>
+      <div class="popup__box--middle">
+        아직 가입하지 않은 팀이 있습니다. <br />
+          확인하시겠습니까?
+      </div>
 
-      <ul class="info--list">
-        <li>원활한 서비스 이용을 위하여 개인정보 제3자 제공에 대하여 동의가 필요합니다. 동의하지 않으실 경우 서비스 이용이 제한됩니다.</li>
-      </ul>
     </template>
     <template v-slot:btn>
       <div class="btn-area ty03">
-        <CustomButton size="sm" color="light">동의하지 않음</CustomButton>
-        <CustomButton size="sm">동의</CustomButton>
+        <CustomButton size="sm" color="light">다시 보지 않기</CustomButton>
+        <CustomButton size="sm">확인하러 가기</CustomButton>
       </div>
     </template>
   </PopupDefault> 
