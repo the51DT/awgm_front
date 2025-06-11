03192e674d887d847fca9281766246c52865916a
suzy hong <victoria1230120@gmail.com>
2025-06-11 17:49:52 +0900

    팀원인증현황보기 UI 수정

--- src/views/Gut00013.vue
+++ src/views/Gut00013.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="content-area">
-    <ContentHead title="팀원 인증 현황보기" headType="ty02" :leftArea=true />
+    <ContentHead title="팀원 인증 현황보기" headType="ty02" :leftArea=true :rightArea=true :sortIcon=true />
     <div class="team-box-wrap">
-      <div class="list-top-tag">
-        <div class="tag-box-wrap">
-          <div class="tag--wrap">
-            <button type="button" class="tag on">이달의 랭킹</button>
-            <button type="button" class="tag">전체인증 횟수</button>
-            <button type="button" class="tag">가입일</button>
-          </div>
-          <div class="tag--wrap">
-            <button type="button" class="order--icon"></button>
-            <button type="button" class="order--icon on"></button> <!-- on 클래스 활성화인 경우(오름차순 정렬) / on 없을 경우(내림차순 정렬) -->
-          </div>
-        </div>
-      </div>
       <div class="tag_cont on">
         <div class="list-top-wrap">
           <div class="filter">
@@ -23,7 +10,20 @@
               :options="['전체', 'ABO 미인증 팀원', '오늘 인증한 팀원', '80% 이상 달성 팀원', '80% 미만 달성 팀원']" />
           </div>
         </div>
-        <div class="list-conts-wrap" v-for="(item, index) in profile" :key="index">
+        <div class="list-btn-wrap">
+        <!-- 
+          1. *높은 랭킹순 텍스트 추가 및 선택 시 텍스트 교체되야함
+          2. 즐겨찾기 / 리스트 뷰 선택 버튼 추가
+        -->
+          <span> &#183; 높은 랭킹순</span>
+          <div class="list-btn-wrap-sort-list">
+            <div> <BookMarkBtn /> </div>
+            <div  class="list-view"> 
+              <button  class="list-view__btn" @click="toggleListView" :class="{ on: isActive == true }"> </button>
+            </div>
+          </div>
+        </div>
+        <div class="list-conts-wrap">
           <div class="card card--border card--p16 card--shadowc2 card--link">
             <BookMarkBtn />
             <button class="card__linkBtn">
@@ -31,39 +31,305 @@
             </button>
             <div class="card__top">
               <div class="card__top__profile_img">
-                <ProfileImg :data="item" />
+                <div class="profile_img">
+                  <div class="profile_img__info">
+                    <div class="user_img-wrap on-flag">
+                      <div class="user_img">
+                        <img :src="require(`@/assets/images/card_profile_1.png`)" alt="프로필 이미지" />
+                      </div>
+                    </div>
+                    <p class="user__tit"> 팀장 김웨이 </p>
+                  </div>
+                </div>
               </div>
               <div class="card__text__list ty02">
                 <dl>
                   <dt>랭킹</dt>
-                  <dd>{{ index + 1 }}위</dd>
+                  <dd>1위</dd>
                 </dl>
                 <dl>
                   <dt>이름</dt>
-                  <dd>{{ item.name }}</dd>
+                  <dd>김웨이</dd>
                 </dl>
                 <dl>
-                  <dt>인증 횟수</dt>
+                  <dt>전체 인증 횟수</dt>
                   <dd>
                     <div class="group-info">
-                      <p class="main">{{ item.count }}/1000회</p>
-                      <p class="sub">($9$월 30회)</p>
+                      <p class="main">999회</p>
                     </div>
                   </dd>
                 </dl>
                 <dl class="align-ctr">
-                  <dt>인증률</dt>
+                  <dt>이 달의 인증</dt>
                   <dd>
-                    <p class="green"> {{ item.rate }}%</p>
+                    <p> 9월 22회 <span class="green"> 60%</span> </p>
                   </dd>
+                </dl>
+                <dl>
+                  <dt> 오늘 인증</dt>
+                  <dd> <span class="green"> O </span></dd>
+                </dl>
+                <dl>
+                  <div class="challenge__badge-wrap">
+                    <div class="challenge__badge badge--abo">
+                      <img src="@/assets/images/ico-abo-completed-badge.svg" alt="ABO 인증 완료 뱃지 아이콘" />
+                      <span> ABO 인증 완료 </span>
+                    </div>
+                    <div class="challenge__badge badge--purchase">
+                      <img src="@/assets/images/ico-purchase-badge.svg" alt="것모닝 구매 완료 아이콘" />
+                      <span> 것모닝 구매 완료 </span>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+
+          <div class="card card--border card--p16 card--shadowc2 card--link">
+            <BookMarkBtn />
+            <button class="card__linkBtn">
+              <img :src="require(`@/assets/images/icon_arrow_r.svg`)" />
+            </button>
+            <div class="card__top">
+              <div class="card__top__profile_img">
+                <div class="profile_img">
+                  <div class="profile_img__info">
+                    <div class="user_img-wrap on-flag">
+                      <div class="user_img">
+                        <img :src="require(`@/assets/images/card_profile_1.png`)" alt="프로필 이미지" />
+                      </div>
+                    </div>
+                    <p class="user__tit"> 김웨이 </p>
+                  </div>
+                </div>
+              </div>
+              <div class="card__text__list ty02">
+                <dl>
+                  <dt>랭킹</dt>
+                  <dd>2위</dd>
+                </dl>
+                <dl>
+                  <dt>이름</dt>
+                  <dd>김웨이</dd>
+                </dl>
+                <dl>
+                  <dt>전체 인증 횟수</dt>
+                  <dd>
+                    <div class="group-info">
+                      <p class="main">999회</p>
+                    </div>
+                  </dd>
+                </dl>
+                <dl class="align-ctr">
+                  <dt>이 달의 인증</dt>
+                  <dd>
+                    <p> 9월 22회 <span class="green"> 60%</span> </p>
+                  </dd>
+                </dl>
+                <dl>
+                  <dt> 오늘 인증</dt>
+                  <dd> <span class="green"> O </span></dd>
+                </dl>
+                <dl>
+                  <div class="challenge__badge-wrap">
+                    <div class="challenge__badge badge--abo">
+                      <img src="@/assets/images/ico-abo-completed-badge.svg" alt="ABO 인증 완료 뱃지 아이콘" />
+                      <span> ABO 인증 완료 </span>
+                    </div>
+                    <div class="challenge__badge badge--purchase">
+                      <img src="@/assets/images/ico-purchase-badge.svg" alt="것모닝 구매 완료 아이콘" />
+                      <span> 것모닝 구매 완료 </span>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+
+          <div class="card card--border card--p16 card--shadowc2 card--link">
+            <BookMarkBtn />
+            <button class="card__linkBtn">
+              <img :src="require(`@/assets/images/icon_arrow_r.svg`)" />
+            </button>
+            <div class="card__top">
+              <div class="card__top__profile_img">
+                <div class="profile_img">
+                  <div class="profile_img__info">
+                    <div class="user_img-wrap on-flag">
+                      <div class="user_img">
+                        <img :src="require(`@/assets/images/card_profile_1.png`)" alt="프로필 이미지" />
+                      </div>
+                    </div>
+                    <p class="user__tit"> 김웨이 </p>
+                  </div>
+                </div>
+              </div>
+              <div class="card__text__list ty02">
+                <dl>
+                  <dt>랭킹</dt>
+                  <dd>2위</dd>
+                </dl>
+                <dl>
+                  <dt>이름</dt>
+                  <dd>김웨이</dd>
+                </dl>
+                <dl>
+                  <dt>전체 인증 횟수</dt>
+                  <dd>
+                    <div class="group-info">
+                      <p class="main">999회</p>
+                    </div>
+                  </dd>
+                </dl>
+                <dl class="align-ctr">
+                  <dt>이 달의 인증</dt>
+                  <dd>
+                    <p> 9월 22회 <span class="green"> 60%</span> </p>
+                  </dd>
+                </dl>
+                <dl>
+                  <dt> 오늘 인증</dt>
+                  <dd> <span class="green"> O </span></dd>
+                </dl>
+                <dl>
+                  <div class="challenge__badge-wrap">
+                    <div class="challenge__badge badge--abo">
+                      <img src="@/assets/images/ico-abo-completed-badge.svg" alt="ABO 인증 완료 뱃지 아이콘" />
+                      <span> ABO 인증 완료 </span>
+                    </div>
+                    <div class="challenge__badge badge--purchase">
+                      <img src="@/assets/images/ico-purchase-badge.svg" alt="것모닝 구매 완료 아이콘" />
+                      <span> 것모닝 구매 완료 </span>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+
+          <div class="card card--border card--p16 card--shadowc2 card--link">
+            <BookMarkBtn />
+            <button class="card__linkBtn">
+              <img :src="require(`@/assets/images/icon_arrow_r.svg`)" />
+            </button>
+            <div class="card__top">
+              <div class="card__top__profile_img">
+                <div class="profile_img">
+                  <div class="profile_img__info">
+                    <div class="user_img-wrap on-flag">
+                      <div class="user_img">
+                        <img :src="require(`@/assets/images/card_profile_1.png`)" alt="프로필 이미지" />
+                      </div>
+                    </div>
+                    <p class="user__tit"> 김웨이 </p>
+                  </div>
+                </div>
+              </div>
+              <div class="card__text__list ty02">
+                <dl>
+                  <dt>랭킹</dt>
+                  <dd>2위</dd>
+                </dl>
+                <dl>
+                  <dt>이름</dt>
+                  <dd>김웨이</dd>
+                </dl>
+                <dl>
+                  <dt>전체 인증 횟수</dt>
+                  <dd>
+                    <div class="group-info">
+                      <p class="main">999회</p>
+                    </div>
+                  </dd>
+                </dl>
+                <dl class="align-ctr">
+                  <dt>이 달의 인증</dt>
+                  <dd>
+                    <p> 9월 22회 <span class="green"> 60%</span> </p>
+                  </dd>
+                </dl>
+                <dl>
+                  <dt> 오늘 인증</dt>
+                  <dd> <span class="green"> O </span></dd>
+                </dl>
+                <dl>
+                  <div class="challenge__badge-wrap">
+                    <div class="challenge__badge badge--abo">
+                      <img src="@/assets/images/ico-abo-completed-badge.svg" alt="ABO 인증 완료 뱃지 아이콘" />
+                      <span> ABO 인증 완료 </span>
+                    </div>
+                    <div class="challenge__badge badge--purchase">
+                      <img src="@/assets/images/ico-purchase-badge.svg" alt="것모닝 구매 완료 아이콘" />
+                      <span> 것모닝 구매 완료 </span>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+
+          <div class="card card--border card--p16 card--shadowc2 card--link">
+            <BookMarkBtn />
+            <button class="card__linkBtn">
+              <img :src="require(`@/assets/images/icon_arrow_r.svg`)" />
+            </button>
+            <div class="card__top">
+              <div class="card__top__profile_img">
+                <div class="profile_img">
+                  <div class="profile_img__info">
+                    <div class="user_img-wrap on-flag">
+                      <div class="user_img">
+                        <img :src="require(`@/assets/images/card_profile_1.png`)" alt="프로필 이미지" />
+                      </div>
+                    </div>
+                    <p class="user__tit"> 김웨이 </p>
+                  </div>
+                </div>
+              </div>
+              <div class="card__text__list ty02">
+                <dl>
+                  <dt>랭킹</dt>
+                  <dd>2위</dd>
+                </dl>
+                <dl>
+                  <dt>이름</dt>
+                  <dd>김웨이</dd>
+                </dl>
+                <dl>
+                  <dt>전체 인증 횟수</dt>
+                  <dd>
+                    <div class="group-info">
+                      <p class="main">999회</p>
+                    </div>
+                  </dd>
+                </dl>
+                <dl class="align-ctr">
+                  <dt>이 달의 인증</dt>
+                  <dd>
+                    <p> 9월 22회 <span class="green"> 60%</span> </p>
+                  </dd>
+                </dl>
+                <dl>
+                  <dt> 오늘 인증</dt>
+                  <dd> <span class="green"> O </span></dd>
+                </dl>
+                <dl>
+                  <div class="challenge__badge-wrap">
+                    <div class="challenge__badge badge--abo">
+                      <img src="@/assets/images/ico-abo-completed-badge.svg" alt="ABO 인증 완료 뱃지 아이콘" />
+                      <span> ABO 인증 완료 </span>
+                    </div>
+                    <div class="challenge__badge badge--purchase">
+                      <img src="@/assets/images/ico-purchase-badge.svg" alt="것모닝 구매 완료 아이콘" />
+                      <span> 것모닝 구매 완료 </span>
+                    </div>
+                  </div>
                 </dl>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="tag_cont">전체인증 횟수</div>
-      <div class="tag_cont">가입일</div>
     </div>
   </div>
   <TopButton></TopButton>
@@ -74,10 +340,7 @@ import ContentHead from "@/components/ContentHead.vue";
 import InputField from "@/components/InputField.vue";
 import TopButton from "@/components/TopButton.vue"
 import BookMarkBtn from "@/components/BookMarkBtn.vue"
-import ProfileImg from "@/components/ProfileImg.vue";
 import tabFn from "@/plugins/common.js";
-
-import { profileList } from '@/router/profile.js'
 
 export default {
   name: "Gut_00013",
@@ -85,7 +348,7 @@ export default {
     return {
       leftArea: "",
       rightArea: "",
-      profile: profileList
+      isActive : false,
     };
   },
   components: {
@@ -93,13 +356,14 @@ export default {
     InputField,
     TopButton,
     BookMarkBtn,
-    ProfileImg
   },
   mounted() {
     tabFn.tabFn()
   },
-  methods: {
-
+  methods: {  
+    toggleListView() {
+      this.isActive = !this.isActive
+    }
   }
 };
 </script>
