66b5eda41ea698b3184e1ef7513d38f9ad11621b
suzy hong <victoria1230120@gmail.com>
2025-06-12 10:25:01 +0900

    임의 컴포넌트 삭제

--- src/views/Gut00018.vue
+++ src/views/Gut00018.vue
@@ -2,13 +2,23 @@
   <div class="content-area">
     <ContentHead title="전체 팀원 보기" :leftArea=true headType="ty02" />
     <div class="team-box-wrap">
-      <div class="list-conts-wrap" v-for="(item, index) in profile" :key="index">
+      <div class="list-conts-wrap">
         <div class="card card--border card--p16 card--shadowc2 card--link">
           <!-- 강퇴버튼 -->
-          <button type="button" class="kickout-btn"  :class="{ 'leaderOn': item.isLeader }"> 강퇴 </button>
+          <button type="button" class="kickout-btn isLeader"> 강퇴 </button>
           <div class="card__top">
-            <ProfileImg :data="item"/>
-            <!-- <p class="card__tit">{{ item.nick }}</p> -->
+            <div class="card__top__profile_img">
+                <div class="profile_img">
+                  <div class="profile_img__info">
+                    <div class="user_img-wrap on-flag">
+                      <div class="user_img">
+                        <img :src="require(`@/assets/images/card_profile_1.png`)" alt="프로필 이미지" />
+                      </div>
+                    </div>
+                    <p class="user__tit"> 팀장 김웨이 </p>
+                  </div>
+                </div>
+              </div>
             <div class="card__text__list ty02 team-list-view">
                 <dl>
                   <dt>팀 가입일</dt>
@@ -27,7 +37,6 @@
 import ContentHead from "@/components/ContentHead.vue";
 import TopButton from "@/components/TopButton.vue";
 import CustomButton from "@/components/CustomButton.vue";
-import ProfileImg from "@/components/ProfileImg.vue";
 
 import { profileList } from '@/router/profile.js'
 
@@ -36,8 +45,7 @@ export default {
   components: {
     ContentHead,
     TopButton,
-    CustomButton,
-    ProfileImg
+    CustomButton, 
   },
   data() {
     return {
