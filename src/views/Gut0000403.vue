e5935b987d67a69e1263e717adf54c0dd869e6e4
suzy hong <victoria1230120@gmail.com>
2025-06-24 10:08:05 +0900

    홈 챌린지 > 인증완료 팝업

---
+++ src/views/Gut0000403.vue
@@ -0,0 +1,40 @@
+<template>
+  <PopupDefault>
+    <template v-slot:title>
+      인증완료
+    </template>
+    <template v-slot:cont>
+      2025년 2월 12일 <br />
+      미션 인증이 완료되었습니다.
+    </template>
+    <template v-slot:btn>
+      <CustomButton size="md">
+        확인
+      </CustomButton>
+    </template>
+  </PopupDefault>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import PopupDefault from "@/components/PopupDefault.vue";
+import CustomButton from "@/components/CustomButton.vue";
+import layerClose from "@/plugins/common.js";
+
+export default {
+  name: "Gut0000403",
+  components: {
+    PopupDefault,
+    CustomButton,
+  },
+  mounted() {
+    layerClose.layerClose("popup");
+  },
+  methods: {
+
+  }
+};
+</script>
