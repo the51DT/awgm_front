18fa183e20679028ac664258e0ec1e023db149e7
SUZY <victoria1230120@gmail.com>
2025-07-24 13:37:25 +0900

    계정 인증 상태 - 인증중입니다 추가

--- src/views/Gut00003.vue
+++ src/views/Gut00003.vue
@@ -16,16 +16,16 @@
           <div class="form-box">
             <div class="input-area">
               <!-- inputField__input--warn 클래스 추가된 경우, error 캡션문구 활성화됨 -->
-              <InputField placeholder="자녀계정 아이디" caption="아이디는 영대소문자(필수), 숫자의 조합으로 10자 이하로 입력해주세요." warn/>
-              <InputField placeholder="자녀계정 닉네임" caption="한글 /영문/숫자 띄어쓰기 포함 20자 이하로입력해주세요." warn/>
-              <InputField type="password" placeholder="자녀계정 비밀번호" caption="비밀번호는 영문, 숫자, 특수문자 중 2종류 조합하여 최소 10자리 이상,또는 3종류를 조합하여 최소 8자리 이상 입력해주세요." warn/>
+              <InputField placeholder="자녀계정 아이디" caption="아이디는 영대소문자(필수), 숫자의 조합으로 10자 이하로 입력해주세요." warn />
+              <InputField placeholder="자녀계정 닉네임" caption="한글 /영문/숫자 띄어쓰기 포함 20자 이하로입력해주세요." warn />
+              <InputField type="password" placeholder="자녀계정 비밀번호"
+                caption="비밀번호는 영문, 숫자, 특수문자 중 2종류 조합하여 최소 10자리 이상,또는 3종류를 조합하여 최소 8자리 이상 입력해주세요." warn />
               <InputField type="password" placeholder="자녀계정 비밀번호 확인" caption="비밀번호가 불일치 합니다." warn />
             </div>
             <div class="radio-wrap">
-              <InputField type="radio" title="" name="radio1" caption="* 만 14세 미만인 경우 부모의 동의가 필요합니다."
-              :radio="[
-                {id: 'radio1_1', value: 'radio1_1', checked: true, label: '만 14세 미만입니다.'},
-                {id: 'radio1_2', value: 'radio1_2', checked: false, label: '만 14세 이상입니다.'},
+              <InputField type="radio" title="" name="radio1" caption="* 만 14세 미만인 경우 부모의 동의가 필요합니다." :radio="[
+                { id: 'radio1_1', value: 'radio1_1', checked: true, label: '만 14세 미만입니다.' },
+                { id: 'radio1_2', value: 'radio1_2', checked: false, label: '만 14세 이상입니다.' },
               ]" />
             </div>
           </div>
@@ -41,7 +41,16 @@
               <CustomButton size="sm">인증하기</CustomButton>
               <!-- 활성화 -->
               <!-- <CustomButton size="sm" color="green">인증하기</CustomButton> -->
-              <p class="label-txt">인증이 완료되었습니다.</p>
+
+              <!-- 인증 완료 시 -->
+              <!-- <p class="label-txt">인증이 완료되었습니다.</p> -->
+              <!-- 인증 중일 시  -->
+              <div class="flex-div">
+                <div class="content-etc-img-area active-small">
+                  <img :src="require('@/assets/images/icon/ico_etc02_loading.png')" alt="로딩페이지 아이콘">
+                </div>
+                <p class="label-txt">인증 중 입니다...</p>
+              </div>
             </div>
             <div class="btn-area">
               <!-- ty-chk on 클래스 추가시, active 처리됨 : 미체크가 디폴트 /
@@ -49,13 +58,13 @@
               <CustomButton color="dark" class="ty-chk on"><span>자녀 개인정보 수집 이용동의</span></CustomButton>
               <!-- <CustomButton color="dark" class="ty-chk on"><span>자녀 개인정보 수집 이용동의</span></CustomButton>  -->
               <!-- 1017 추가 : 기획 변경 해당 화면에 추가 버튼 생성됨 -->
-              <CustomButton color="dark" class="ty-chk"><span>개인정보 제3자 제공 이용동의</span></CustomButton> 
-              <CustomButton color="dark" class="ty-chk"><span>이용약관 동의</span></CustomButton> 
+              <CustomButton color="dark" class="ty-chk"><span>개인정보 제3자 제공 이용동의</span></CustomButton>
+              <CustomButton color="dark" class="ty-chk"><span>이용약관 동의</span></CustomButton>
             </div>
           </div>
           <div class="bdr-gray"></div>
           <div class="form-box">
-            <div class="btn-area"> 
+            <div class="btn-area">
               <!-- 기본 버튼 케이스 -->
               <CustomButton>등록완료</CustomButton>
               <!-- disabled 버튼 케이스 -->
@@ -68,6 +77,15 @@
     </template>
   </PopupDefault>
 </template>
+
+<style scoped>
+.flex-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+}
+</style>
 
 <script>
 
