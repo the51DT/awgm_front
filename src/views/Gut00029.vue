34d786efe25d8d41757321471455c329cf81d991
SUZY <victoria1230120@gmail.com>
2025-07-29 15:40:27 +0900

    로딩 스크롤 되는 문제
    - 스크롤 막은거 X 그렇게 보이게만 수정

--- src/views/Gut00029.vue
+++ src/views/Gut00029.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content-etc-wrap"> 
+  <div class="content-etc-wrap loading">
     <div class="content-etc-box">
       <div class="content-etc-img-area active">
         <img :src="require('@/assets/images/icon/ico_etc02_loading.png')" alt="로딩페이지 아이콘">
@@ -9,10 +9,10 @@
           Loading...
         </h2>
         <p class="page-desc">
-          데이터를 불러오고 있습니다. <br/>
+          데이터를 불러오고 있습니다. <br />
           잠시만 기다려 주세요.
         </p>
-      </div>      
+      </div>
     </div>
   </div>
 </template>
@@ -21,7 +21,7 @@
 
 export default {
   name: "Gut_00029",
-  components: {  
+  components: {
 
   }
 };
