f8e112c296d8ff143a2333dfb8c3c9f51b03d7b2
SUZY <victoria1230120@gmail.com>
2025-07-30 17:07:57 +0900

    챌린지 이력보기 마이웰니스버전 제작
    - 카드 arrow 00023 포함 모두 icon_arrow_r.svg로 교체

--- src/views/Gut10023.vue
+++ src/views/Gut10023.vue
@@ -1,11 +1,5 @@
 <template v-slot:full>
-  <!-- 
-    n회차 진행, 
-    성공보상 부분 텍스트 컬러와 아까 말씀드린 챌린지, 
-    인원 뱃지 텍스트 크기와 형태, 크기도 일부 변경되어 함께 확인
-  -->
   <ContentHead title="챌린지 이력보기" headType="ty02" :leftArea="true" />
-  <!-- 1025 수정 : 챌린지 nodata 케이스 추가 -->
   <!-- 챌린지 이력보기 데이터 존재하는 경우 -->
   <div v-if="challengeData" class="popup--full__cont pb-50">
     <div class="card card--border card--p16 card--shadowc2 challenge__card--history">
@@ -45,42 +39,21 @@
           <p class="success">성공</p>
         </div>
       </div>
-    </div>
-    <div class="card card--border card--p16 card--shadowc2 challenge__card--history">
-      <button class="card__linkBtn">
-        <img :src="require(`@/assets/images/icon_arrow_r.svg`)">
-      </button>
-      <p class="font--800 font--c11 font--ls1 font--size18">GUT Morning Korea Challenge</p>
-      <div class="card__top">
-        <div class="card__img">
-          <img :src="require(`@/assets/images/challenge_profile.png`)">
-        </div>
-        <div class="card__text">
+      <div>
+        <p class="font--800 font--ls1 center"> 노화 속도 개선 추가 미션 결과</p>
+        <div class="challenge__card--history--bottom">
           <div>
-            <p class="font--800 font--ls1"> <span> 팀명 </span> 아침마다 오 마이 GUT Morning</p>
-            <div class="card__info">
-              <div class="card__title card__title--challenge">챌린지</div>
-              <div class="card__num">64명</div>
-            </div>
+            <p>1차 분석 결과</p>
+            <p> x 1.84</p>
           </div>
-          <div class="card__date grid">
-            <p> <strong class="font-800"> 2회차 진행 </strong></p>
-            <p> 2025.09.01 ~ 2025.09.30 / (70일) </p>
+          <div>
+            <p>2차 분석 결과</p>
+            <p>x 1.73</p>
           </div>
-        </div>
-      </div>
-      <div class="challenge__card--history--bottom">
-        <div>
-          <p>나의 인증률</p>
-          <p>100%</p>
-        </div>
-        <div>
-          <p>팀 인증률</p>
-          <p>100%</p>
-        </div>
-        <div>
-          <p>챌린지 결과</p>
-          <p class="success">성공</p>
+          <div>
+            <p>노화 속도 개선</p>
+            <p class="success">성공</p>
+          </div>
         </div>
       </div>
     </div>
@@ -121,6 +94,23 @@
           <p class="success">성공</p>
         </div>
       </div>
+      <div>
+        <p class="font--800 font--ls1 center"> 노화 속도 개선 추가 미션 결과</p>
+        <div class="challenge__card--history--bottom">
+          <div>
+            <p>1차 분석 결과</p>
+            <p> x 1.84</p>
+          </div>
+          <div>
+            <p>2차 분석 결과</p>
+            <p>x 1.73</p>
+          </div>
+          <div>
+            <p>노화 속도 개선</p>
+            <p class="success">성공</p>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="card card--border card--p16 card--shadowc2 challenge__card--history">
       <button class="card__linkBtn">
@@ -159,8 +149,79 @@
           <p class="fail">실패</p>
         </div>
       </div>
+      <div>
+        <p class="font--800 font--ls1 center"> 노화 속도 개선 추가 미션 결과</p>
+        <div class="challenge__card--history--bottom">
+          <div>
+            <p>1차 분석 결과</p>
+            <p> x 1.84</p>
+          </div>
+          <div>
+            <p>2차 분석 결과</p>
+            <p>x 1.73</p>
+          </div>
+          <div>
+            <p>노화 속도 개선</p>
+            <p class="fail">실패</p>
+          </div>
+        </div>
+      </div>
     </div>
-
+    <div class="card card--border card--p16 card--shadowc2 challenge__card--history">
+      <button class="card__linkBtn">
+        <img :src="require(`@/assets/images/icon_arrow_r.svg`)">
+      </button>
+      <p class="font--800 font--c11 font--ls1 font--size18">GUT Morning Korea Challenge</p>
+      <div class="card__top">
+        <div class="card__img">
+          <img :src="require(`@/assets/images/challenge_profile.png`)">
+        </div>
+        <div class="card__text">
+          <div>
+            <p class="font--800 font--ls1"> <span> 팀명 </span> 아침마다 오 마이 GUT Morning</p>
+            <div class="card__info">
+              <div class="card__title card__title--challenge">챌린지</div>
+              <div class="card__num">64명</div>
+            </div>
+          </div>
+          <div class="card__date grid">
+            <p> <strong class="font-800"> 2회차 진행 </strong></p>
+            <p> 2025.09.01 ~ 2025.09.30 / (70일) </p>
+          </div>
+        </div>
+      </div>
+      <div class="challenge__card--history--bottom">
+        <div>
+          <p>나의 인증률</p>
+          <p>100%</p>
+        </div>
+        <div>
+          <p>팀 인증률</p>
+          <p>100%</p>
+        </div>
+        <div>
+          <p>챌린지 결과</p>
+          <p class="fail">실패</p>
+        </div>
+      </div>
+      <div>
+        <p class="font--800 font--ls1 center"> 노화 속도 개선 추가 미션 결과</p>
+        <div class="challenge__card--history--bottom">
+          <div>
+            <p>1차 분석 결과</p>
+            <p> x 1.84</p>
+          </div>
+          <div>
+            <p>2차 분석 결과</p>
+            <p>x 1.73</p>
+          </div>
+          <div>
+            <p>노화 속도 개선</p>
+            <p class="fail">실패</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
   <!-- 챌린지 이력보기 데이터 존재하지 않는 경우 -->
@@ -172,12 +233,32 @@
   </div>
 </template>
 
+<style scoped>
+.card__linkBtn {
+  top: 30%;
+}
+
+.card__date.grid {
+  grid-template-columns: 1fr;
+  gap: 4px;
+}
+
+p.font--800.font--ls1.center {
+  text-align: center;
+  margin: 4px 0;
+}
+
+strong.font-800 {
+  color: #169E4A;
+}
+</style>
+
 <script>
 import ContentHead from "@/components/ContentHead.vue";
 
 
 export default {
-  name: "Gut00023",
+  name: "Gut10023",
   props: {
     challengeData: {
       type: Boolean,
