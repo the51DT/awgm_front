5d4c6ccd530101ff85bf05cf44a8a173aedd9b93
SUZY <victoria1230120@gmail.com>
2025-08-05 15:18:05 +0900

    팀원인증현황보기 input 크기 조절
    filter클래스에 '.half' 이중클래스 추가해서 5:5 비율 맞춤

--- src/views/Gut00013-08.vue
+++ src/views/Gut00013-08.vue
@@ -5,17 +5,19 @@
     <div class="team-box-wrap">
       <div class="tag_cont on">
         <div class="list-top-wrap">
-          <div class="filter">
-            <InputField type="select" :options="['2024.09', '2024.08', '2024.07']" active v-model="selectedDate"/>
+          <div class="filter half">
+            <InputField type="select" :options="['2024.09', '2024.08', '2024.07']" active v-model="selectedDate" />
             <InputField type="select" v-model="selectedMember" active
-              :options="['전체', 'ABO 미인증 팀원', '오늘 인증 완료','오늘 미인증 팀원', '80% 이상 달성', '80% 미만 달성']" />
+              :options="['전체', 'ABO 미인증 팀원', '오늘 인증 완료', '오늘 미인증 팀원', '80% 이상 달성', '80% 미만 달성']" />
           </div>
         </div>
         <div class="list-btn-wrap">
           <span> 높은 랭킹순</span>
           <div class="list-btn-wrap-sort-list">
-            <div> <BookMarkBtn /> </div>
-            <div class="list-view"> 
+            <div>
+              <BookMarkBtn />
+            </div>
+            <div class="list-view">
               <div>
                 <button class="list-view__btn" @click="toggleListView" :class="{ on: isActive == true }"> </button>
               </div>
@@ -47,9 +49,9 @@ export default {
     return {
       leftArea: "",
       rightArea: "",
-      isActive : false,
+      isActive: false,
       selectedDate: '2024.08',
-      selectedMember :'80% 미만 달성'
+      selectedMember: '80% 미만 달성'
     };
   },
   components: {
@@ -61,7 +63,7 @@ export default {
   mounted() {
     tabFn.tabFn()
   },
-  methods: {  
+  methods: {
     toggleListView() {
       this.isActive = !this.isActive
     }
